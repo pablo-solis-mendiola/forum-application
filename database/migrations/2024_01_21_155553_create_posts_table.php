72cf8cac144c5e472422854b82414ad094ebb262
Pablo Jonhatan Solis Mendiola <pablosolismendiola@gmail.com>
2024-01-22 09:59:18 -0600

    Create migrations for the application

---
+++ database/migrations/2024_01_21_155553_create_posts_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 150);
+            $table->string('slug', 160)->unique();
+            $table->foreignId('poster_id')->nullable()->constrained('users')->nullOnDelete()->cascadeOnUpdate();
+            $table->timestamp('posted_at');
+            $table->timestamp('updated_at')->nullable();
+            $table->timestamp('archived_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
---
+++ database/migrations/2024_01_21_155554_create_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('commentable');
+            $table->foreignId('poster_id')->nullable()->constrained('users')->nullOnDelete()->cascadeOnUpdate();
+            $table->timestamp('posted_at');
+            $table->timestamp('updated_at')->nullable()->cascadeOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
